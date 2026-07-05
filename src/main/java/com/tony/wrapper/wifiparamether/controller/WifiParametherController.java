1c5b7c2ae5fd644ce37eba635129f321f8aeea22
loop <tony.kusic@gmail.com>
2026-07-05 10:48:32 +0200

    404 response

--- src/main/java/com/tony/wrapper/wifiparamether/controller/WifiParametherController.java
+++ src/main/java/com/tony/wrapper/wifiparamether/controller/WifiParametherController.java
@@ -1,5 +1,6 @@
 package com.tony.wrapper.wifiparamether.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.tony.wrapper.wifiparamether.WifiParametherConfigService;
 import com.tony.wrapper.wifiparamether.dto.PutWifiParametherRequest;
 import com.tony.wrapper.wifiparamether.dto.PutWifiParametherResponse;
 import com.tony.wrapper.wifiparamether.dto.WifiParametherResponse;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 @RestController
 @Validated
@@ -27,7 +29,11 @@ public class WifiParametherController {
 
     @GetMapping("/wifi-parameter/{cpeId}")
     public ResponseEntity<WifiParametherResponse> getWifiParameter(@PathVariable @NotBlank String cpeId) {
-        return ResponseEntity.ok(this.wifiParametherConfigService.getCpeId(cpeId));
+        WifiParametherResponse response = this.wifiParametherConfigService.getCpeId(cpeId);
+        if (response == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No wifi configuration found for cpeId: " + cpeId);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/wifi-parameter")
